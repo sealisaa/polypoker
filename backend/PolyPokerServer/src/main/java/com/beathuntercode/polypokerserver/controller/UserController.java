33aef7410b25eb5cc013b6bebe33d1280e0abe29
beatHunteRcode <nikitasherepa@gmail.com>
2022-12-11 20:28:16 +0300

    Fix findUserByLogin method GET-request

--- backend/PolyPokerServer/src/main/java/com/beathuntercode/polypokerserver/controller/UserController.java
+++ backend/PolyPokerServer/src/main/java/com/beathuntercode/polypokerserver/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,11 +23,11 @@ public class UserController {
         return userDao.getAllUsers();
     }
 
-    @GetMapping("/user/get-by-login")
-    public User getUserByLogin(@RequestBody String login) {
+    @GetMapping("/user/{login}")
+    public User getUserByLogin(@PathVariable String login) {
         return userDao.getUserByLogin(login);
     }
-
+    
     @PostMapping("/user/save")
     public User saveUser(@RequestBody User user) {
         return userDao.save(user);
