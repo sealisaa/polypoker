2f749c64ef062a47d4636e820959269c8887045f
beatHunteRcode <nikitasherepa@gmail.com>
2022-12-13 18:34:15 +0300

    #18: Add password encryption on registration

--- backend/PolyPokerServer/src/main/java/com/beathuntercode/polypokerserver/model/user/UserDao.java
+++ backend/PolyPokerServer/src/main/java/com/beathuntercode/polypokerserver/model/user/UserDao.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Streamable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.beathuntercode.polypokerserver.model.userstatistic.UserStatistic;
@@ -20,14 +21,23 @@ public class UserDao {
     @Autowired
     private UserStatisticRepository userStatisticRepository;
 
-    public User save(User user) {
+    public User save(User u) {
         UserStatistic userStatistic = new UserStatistic();
-        userStatistic.setLogin(user.getLogin());
+        userStatistic.setLogin(u.getLogin());
         userStatistic.setCurrentCoinsCount(5000);
         userStatistic.setTotalGamesPlayed(0);
         userStatistic.setWinGames(0);
         userStatistic.setTotalEarn(0);
         userStatisticRepository.save(userStatistic);
+
+        User user = new User();
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        user.setLogin(u.getLogin());
+        user.setPassword(encoder.encode(u.getPassword()));
+        user.setName(u.getName());
+        user.setSurname(u.getSurname());
+
         return userRepository.save(user);
     }
 
