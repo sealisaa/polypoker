41416aa44867f54daa8574facffb02c2683adac3
beatHunteRcode <nikitasherepa@gmail.com>
2023-01-05 23:18:26 +0300

    Add CrossOrigin annotation

--- backend/PolyPokerServer/src/main/java/com/beathuntercode/polypokerserver/database/controller/UserController.java
+++ backend/PolyPokerServer/src/main/java/com/beathuntercode/polypokerserver/database/controller/UserController.java
@@ -8,10 +8,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.beathuntercode.polypokerserver.database.model.user.User;
 import com.beathuntercode.polypokerserver.database.model.user.UserDao;
 
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class UserController {
 
