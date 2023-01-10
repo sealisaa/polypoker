5e89c8d0f521ff8b666d0b52b7a9d3b0809bdf6f
Алиса <sealisaa@yandex.ru>
2023-01-10 14:27:45 +0300

    add modal with success register message

--- web/src/components/Register.js
+++ web/src/components/Register.js
@@ -3,13 +3,42 @@ import '../style/style.css';
 import UserService from '../services/UserService';
 import { Navigate } from "react-router-dom";
 
+const Modal = ({visible = false, title = '', content = '', footer = '', onClose}) => {
+    const onKeydown = ({ key }: KeyboardEvent) => {
+        switch (key) {
+            case 'Escape':
+                onClose()
+                break
+        }
+    }
+    React.useEffect(() => {
+        document.addEventListener('keydown', onKeydown)
+        return () => document.removeEventListener('keydown', onKeydown)
+    })
+    if (!visible) {
+        return null
+    }
+    return (
+        <div className='modal' onClick={onClose}>
+            <div className='modal-dialog' onClick={e => e.stopPropagation()}>
+                <div className='modal-header'>
+                    <h3 className='modal-title'>{title}</h3>
+                </div>
+                {footer && <div className='modal-footer'>{footer}</div>}
+            </div>
+        </div>
+    )
+}
+
 class Register extends React.Component {
     constructor(props) {
         super(props);
         this.saveUser = this.saveUser.bind(this);
+        this.onClose = this.onClose.bind(this);
         this.state = {
             message: "",
-            saved: false
+            saved: false,
+            isModal: false
         }
     }
 
@@ -21,16 +50,19 @@ class Register extends React.Component {
         let password2 = document.getElementById("password2").value;
         if (password !== password2) {
             this.setState({message: "Пароли не совпадают"});
-            return;
         } else {
-            this.setState({message: " "});
             UserService.saveUser(name, surname, login, password).then((response) => {
-                this.setState({saved: true});
+                this.setState({message: " ", isModal: true});
             }).catch(err => {
                 this.setState({message: "Что-то пошло не так"});
                 console.log(err)
             });
+
         }
+    }
+
+    onClose() {
+        this.setState({isModal: false, saved: true});
     }
 
     render() {
@@ -39,6 +71,12 @@ class Register extends React.Component {
         }
         return (
             <div className="register__main">
+                <Modal
+                    visible={this.state.isModal}
+                    title='Пользователь успешно зарегистрирован'
+                    footer={<button className="button" onClick={this.onClose}>Ок</button>}
+                    onClose={this.onClose}
+                />
                 <div className="register__left">
                     <h2>Регистрация</h2>
                     <div className="register__name">
