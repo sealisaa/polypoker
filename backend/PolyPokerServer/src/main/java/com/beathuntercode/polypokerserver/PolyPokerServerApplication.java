bcf6771ecceeca8ce64650027a7a6d99492d1ac7
beatHunteRcode <nikitasherepa@gmail.com>
2023-01-27 19:07:59 +0300

    Fix room creation on server start

--- backend/PolyPokerServer/src/main/java/com/beathuntercode/polypokerserver/PolyPokerServerApplication.java
+++ backend/PolyPokerServer/src/main/java/com/beathuntercode/polypokerserver/PolyPokerServerApplication.java
@@ -26,6 +26,7 @@ public class PolyPokerServerApplication {
 //
 //		testHandEvaluation(room, room.getPlayersMap().get("a"));
 
+		Utilities.roomsController.createRoom(1, 10, 20);
 		SpringApplication.run(PolyPokerServerApplication.class, args);
 
 	}
